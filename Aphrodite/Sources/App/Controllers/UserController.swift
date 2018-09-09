8021ad85d7e2db5efbf5e227683e7574ebde3576
wangfei <wangfei@zhihu.com>
2018-09-09 22:47:45 +0800

    route and model

---
+++ Aphrodite/Sources/App/Controllers/UserController.swift
@@ -0,0 +1,20 @@
+//
+//  UserController.swift
+//  App
+//
+//  Created by Wang Jianwei on 2018/9/9.
+//
+import Vapor
+
+final class UserController {
+    static func users(_ req: Request) throws -> Future<[User]> {
+        return User.query(on: req).all()
+    }
+    static func addUser(_ req: Request) throws -> Future<[String: String]> {
+        return try req.content.decode(User.self).flatMap {
+            return $0.save(on: req).map { _ in
+                return ["code": "200"]
+            }
+        }
+    }
+}
---
+++ Aphrodite/Sources/App/Models/Response.swift
@@ -0,0 +1,16 @@
+//
+//  Response.swift
+//  App
+//
+//  Created by Wang Jianwei on 2018/9/9.
+//
+
+//import FluentSQLite
+//import Vapor
+//
+//class Response: SQLiteModel {
+//    o
+//}
+//
+//extension Response: Content {}
+//extension Response: Migration {}
